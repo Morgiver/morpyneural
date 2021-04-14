bf743bd76fbb412ae3ca3b9c478fc828db01428e
Morgiver <morgan@morgiver.net>
2021-04-14 16:24:13 +0200

    Add build method in Layer class

--- Neural/LayersClass.py
+++ Neural/LayersClass.py
@@ -1,4 +1,4 @@
-
+from Math.PyMatrix import Py2DMatrix
 
 class Layer:
     def __init__(self):
@@ -8,3 +8,26 @@ class Layer:
         self.weights = None
         self.biases = None
         self.activation = None
+
+    def build(self, inputs, nodes, activation):
+        """
+        Building Weights and Biases matrices, define the Activation Function
+        :param inputs:
+        :param nodes:
+        :param activation:
+        :return:
+        """
+        # Weights PyD2Matrix
+        self.weights = Py2DMatrix(nodes, inputs)
+        self.weights.build()
+        self.weights.randomize(-1, 1)
+
+        # Biases PyD2Matrix
+        self.biases = Py2DMatrix(nodes, 1)
+        self.biases.build()
+        self.biases.randomize(-1, 1)
+
+        # Set Activation
+        self.activation = activation
+
+        return self
