be195833343e1ebfa9f74f5a82ba8815b53e569f
Morgiver <morgan@morgiver.net>
2021-04-14 15:55:32 +0200

    Add sigmoid and relu activation functions

--- Math/PyMatrix.py
+++ Math/PyMatrix.py
@@ -2,6 +2,14 @@ import numpy as np
 import random
 
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
 class Py2DMatrix:
     def __init__(self, rows, cols):
         self.rows = rows
