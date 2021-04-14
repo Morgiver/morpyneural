06d884f4a267c790da504d06e40a153ea98851d5
Morgiver <morgan@morgiver.net>
2021-04-14 18:12:46 +0200

    Add build method in Population class

--- Genetic/PopulationClass.py
+++ Genetic/PopulationClass.py
@@ -4,3 +4,15 @@ from Genetic.ElementClass import Element
 class Population:
     def __init__(self):
         self.elements = []
+
+    def build(self, layers_configuration, max_elements):
+        """
+        Building new set of Elements
+        :param layers_configuration:
+        :param max_elements:
+        :return:
+        """
+        self.elements.clear()
+        for i in range(max_elements):
+            new_element = Element().build(layers_configuration)
+            self.elements.append(new_element)
