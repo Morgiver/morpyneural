1a2e1f2d06875b708bcafacefff3c9e1704b3d56
Morgiver <morgan@morgiver.net>
2021-04-14 16:15:00 +0200

    Create LayersClass.py File, create Layer class

---
+++ Neural/LayersClass.py
@@ -0,0 +1,10 @@
+
+
+class Layer:
+    def __init__(self):
+        """
+        Layer contain Weights, Biases and Activation data's of a Neural Network
+        """
+        self.weights = None
+        self.biases = None
+        self.activation = None
