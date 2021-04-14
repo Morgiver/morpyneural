69b2cd9f07ef0a91a8687419dc45708e94f87ff3
Morgiver <morgan@morgiver.net>
2021-04-14 17:03:07 +0200

    Add build method in NeuralNetwork class

--- Neural/NeuralNetworkClass.py
+++ Neural/NeuralNetworkClass.py
@@ -1,5 +1,18 @@
+from Neural.LayersClass import Layer
 
 
 class NeuralNetwork:
     def __init__(self):
         self.layers = []
+
+    def build(self, layers_config):
+        """
+        Building new Layer Array
+        :param layers_config:
+        :return:
+        """
+        for config in layers_config:
+            new_layer = Layer().build(config['inputs'], config['nodes'], config['activation'])
+            self.layers.append(new_layer)
+
+        return self
