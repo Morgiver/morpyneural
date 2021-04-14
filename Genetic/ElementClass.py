622830e020972aca29d558e9daa4c9a4d56e0a9a
Morgiver <morgan@morgiver.net>
2021-04-14 18:04:03 +0200

    Add build method in Element class

--- Genetic/ElementClass.py
+++ Genetic/ElementClass.py
@@ -3,5 +3,17 @@ from Neural.NeuralNetworkClass import NeuralNetwork
 
 class Element:
     def __init__(self):
+        """
+        Element is a part of Population, it containing the neural network and (will) handle
+        the score logic
+        """
         self.active = True
         self.neural_network = NeuralNetwork()
+
+    def build(self, layers_configuration):
+        """
+        Build the neural network
+        :param layers_configuration:
+        :return:
+        """
+        self.neural_network.build(layers_configuration)
