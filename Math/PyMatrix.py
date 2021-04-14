7df317f1d7d7db01981ca1c65c86fa536310aa8d
Morgiver <morgan@morgiver.net>
2021-04-14 15:14:24 +0200

    Renaming PyMatrix.Matrix to PyMatrix.Py2DMatrix, adding build method

--- Math/PyMatrix.py
+++ Math/PyMatrix.py
@@ -1,7 +1,24 @@
+import numpy as np
 
 
-class Matrix:
+class Py2DMatrix:
     def __init__(self, rows, cols):
         self.rows = rows
         self.cols = cols
-        self.values = []
+        self.values = None
+
+    def build(self, dtype=np.float32):
+        """
+        Building new Matrix
+        :param dtype:
+        :return:
+        """
+        matrix = []
+        for i in range(self.rows):
+            cols_array = []
+            for j in range(self.cols):
+                cols_array.append(0)
+
+            matrix.append(cols_array)
+
+        self.values = np.array(matrix, dtype=dtype)
