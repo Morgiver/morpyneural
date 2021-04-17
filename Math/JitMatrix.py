c4c48c797c3100f26a4536e1d63341f4a3157cc0
Morgiver <morgan@morgiver.net>
2021-04-17 09:40:43 +0200

    Add add, multiply, dot_product and from_array methods

--- Math/JitMatrix.py
+++ Math/JitMatrix.py
@@ -21,3 +21,19 @@ class JitMatrix(object):
     def randomize(self, low, high):
         self.values = np.random.uniform(low, high, size=(self.rows, self.cols))
         return self
+
+    def add(self, value):
+        self.values = np.add(self.values, value)
+        return self
+
+    def multiply(self, value):
+        self.values = np.multiply(self.values, value)
+        return self
+
+    def dot_product(self, jit_matrix):
+        return np.dot(self.values, jit_matrix.values)
+
+    def from_array(self, array_values):
+        self.values = array_values.reshape((len(array_values), 1))
+        return self
+
