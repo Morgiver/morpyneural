4b08891817d0cd4dbf12eb8ce390c03c4efc5afb
Morgiver <morgan@morgiver.net>
2021-04-17 10:28:17 +0200

    Create JitElementClass.py and JitPopulationClass.py Files, create JitElement class and JitPopulation class

---
+++ Genetic/JitPopulationClass.py
@@ -0,0 +1,55 @@
+from numba.experimental import jitclass
+from Genetic.JitElementClass import JitElement
+from Math.JitMatrix import JitMatrix
+
+spec = [
+    ('elements', JitElement[:])
+]
+
+
+@jitclass(spec)
+class JitPopulation(object):
+    def __init__(self):
+        self.elements = []
+
+    def build(self, layers_configuration, max_elements):
+        """
+        Building new set of Elements
+        :param layers_configuration:
+        :param max_elements:
+        :return:
+        """
+        self.elements.clear()
+        for i in range(max_elements):
+            new_element = JitElement().build(layers_configuration)
+            self.elements.append(new_element)
+
+        return self
+
+    def feed_forward(self, inputs):
+        """
+        Feed forwarding all Elements
+        :param inputs:
+        :return:
+        """
+        inputs = JitMatrix(1, 1).from_array(inputs)
+
+        results = []
+        for element in self.elements:
+            results.append(element.feed_forward(inputs))
+
+        return results
+
+    def evolve(self, parent_a, parent_b, learning_rate=0.001):
+        """
+        Evolve each Element
+        :param parent_a:
+        :param parent_b:
+        :param learning_rate:
+        :return:
+        """
+        for element in self.elements:
+            if element != parent_a and element != parent_b:
+                element.evolve(parent_a, parent_b, learning_rate=learning_rate)
+
+        return self
