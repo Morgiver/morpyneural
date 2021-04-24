46bde22b387485ed0904c7783f68e2f09708e973
Morgiver <morgan@morgiver.net>
2021-04-24 10:58:51 +0200

    Adding __init__.py to create the package

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from Genetic.ElementClass import Element
+from Genetic.PopulationClass import Population
+from Genetic.JitElementClass import JitElement, JitElementType, JitElementListType
+from Genetic.JitPopulationClass import JitPopulation, JitPopulationType, JitPopulationListType
+from Math.PyMatrix import Py2DMatrix
+from Math.JitMatrix import JitMatrix, JitMatrixType, JitMatrixListType
+from Neural.LayersClass import Layer
+from Neural.NeuralNetworkClass import NeuralNetwork
+from Neural.JitLayerClass import JitLayer, JitLayerType, JitLayerListType
+from Neural.JitNeuralNetworkClass import JitNeuralNetwork, JitNeuralNetworkType, JitNeuralNetworkListType
--- test.py
+++ test.py
@@ -7,28 +7,7 @@ from Genetic.JitPopulationClass import JitPopulation
 from Math.JitMatrix import JitMatrix
 
 
-@numba.njit
 def main():
-    layers_config = np.array([
-        [2, 4, 1],
-        [4, 2, 1]
-    ], dtype=np.int32)
-
-    element = JitElement()
-    element.build(layers_config)
-
-    print(element.neural_network.layers[0].weights.values)
-    data = np.array([2.0, 5.0], dtype=np.float32)
-    data = JitMatrix.from_array(data)
-    print(element.feed_forward(data).values)
-
-    pop = JitPopulation()
-    pop.build(layers_config, 10)
-    print(pop.elements)
-    print(pop.feed_forward(data))
-
-
-if __name__ == '__main__':
     layers_config = np.array([
         [50, 250, 1],
         [250, 250, 1],
@@ -49,3 +28,7 @@ if __name__ == '__main__':
     print(f"Start at : {datetime.datetime.fromtimestamp(time.time())}")
     print(pop.feed_forward(data))
     print(f"End at : {datetime.datetime.fromtimestamp(time.time())}")
+
+
+if __name__ == '__main__':
+    draw()
