65eb828d2e84f89e9afd7fc688a89794d4d9cc63
Morgiver <morgan@morgiver.net>
2021-04-29 18:43:16 +0200

    update example

--- examples/simple-neuralnetwork.py
+++ examples/simple-neuralnetwork.py
@@ -6,29 +6,6 @@ from morpyneural.Genetic.JitPopulationClass import JitPopulation
 from morpyneural.Math.JitMatrix import JitMatrix
 
 
-def main():
-    layers_config = np.array([
-        [2, 250, 1],
-        [250, 250, 1],
-        [250, 250, 1],
-        [250, 250, 1],
-        [250, 2, 1]
-    ], dtype=np.int32)
-
-    element = JitElement()
-    element.build(layers_config)
-
-    data = np.array([2.0, 5.0], dtype=np.float32)
-    data = JitMatrix.from_array(data)
-
-    pop = JitPopulation()
-    pop.build(layers_config, 250)
-    print(pop.feed_forward(data))
-    print(f"Start at : {datetime.datetime.fromtimestamp(time.time())}")
-    print(pop.feed_forward(data))
-    print(f"End at : {datetime.datetime.fromtimestamp(time.time())}")
-
-
 if __name__ == '__main__':
     layers_config = np.array([
         [2, 250, 1],
